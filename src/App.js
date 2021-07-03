a24f1a32d11ffb44f1c407ecaff68ebd37a852ec
treuther <tracyreuther@gmail.com>
2021-07-03 09:10:08 -0500

    button and onClick event added

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <p>
           You clicked me {count} times!
         </p>
+        <button onClick={() => setCount(count + 1)}>UP the COUNT!</button>
       </header>
     </div>
   );
