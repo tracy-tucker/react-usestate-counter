beac074bcbb1f12e0cd3eeb1793b6c3a6eaf9e44
treuther <tracyreuther@gmail.com>
2021-07-03 09:08:40 -0500

    sentence setup

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Overly-Played Counter</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You clicked me {count} times!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
